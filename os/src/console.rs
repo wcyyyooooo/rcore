08410c7409094a896e8c8f96ede6a4cb263f419f
wcyyyooo <2812001079@qq.com>
2024-04-18 16:41:52 +0800

    first commit

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,24 +1,18 @@
-//! SBI console driver, for text output
-
 use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
-
 struct Stdout;
-
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        for c in s.chars() {
+        for c in s.bytes() {
             console_putchar(c as usize);
         }
         Ok(())
     }
 }
-
 pub fn print(args: fmt::Arguments) {
     Stdout.write_fmt(args).unwrap();
 }
 
-/// print string macro
 #[macro_export]
 macro_rules! print {
     ($fmt: literal $(, $($arg: tt)+)?) => {
@@ -26,10 +20,33 @@ macro_rules! print {
     }
 }
 
-/// println string macro
 #[macro_export]
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
+}
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!("{}{}{}\n", "\x1B[34m[INFO]", format_args!($fmt $(, $($arg)+)?), "\x1B[0m"));
+    };
+}
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!("{}{}{}\n", "\x1B[93m[WARN]", format_args!($fmt $(, $($arg)+)?), "\x1B[0m"));
+    };
+}
+#[macro_export]
+macro_rules! error{
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!("{}{}{}\n", "\x1B[31m[ERROR]", format_args!($fmt $(, $($arg)+)?), "\x1B[0m"));
+    };
+}
+#[macro_export]
+macro_rules! debug{
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!("{}{}{}\n", "\x1B[32m[DEBUG]", format_args!($fmt $(, $($arg)+)?), "\x1B[0m"));
+    };
 }
--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,20 +1,17 @@
-//! The panic handler
-
 use crate::sbi::shutdown;
 use core::panic::PanicInfo;
-use log::*;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     if let Some(location) = info.location() {
-        error!(
-            "[kernel] Panicked at {}:{} {}",
+        println!(
+            "Panicked at {}:{} {}",
             location.file(),
             location.line(),
             info.message().unwrap()
         );
     } else {
-        error!("[kernel] Panicked: {}", info.message().unwrap());
+        println!("Panicked: {}", info.message().unwrap());
     }
     shutdown(true)
 }
